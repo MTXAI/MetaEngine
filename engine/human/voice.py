25409c7cc1d46bc229d1a61d9cb5b3a8ecc0139f
freekatz <katz@mtxai.io>
2025-06-17 21:54:08 +0800

    stage

---
+++ engine/human/voice.py
@@ -0,0 +1,8 @@
+
+## pipeline 文字 - 语音 done 一个 pipeline
+## pipeline 语音 - 文字 done 一个 pipeline
+## pipeline 语音 - 文字 - 文字 done 一个 pipeline
+## pipeline 语音 - 文字 + 文字 - 语音 done, 两个 pipeline
+## pipeline 文字 - 文字 - 语音+视频 - 视频 两个 pipeline + 一个 player 匹配合成语音与视频
+## pipeline 语音 - 文字 - 文字 - 语音+视频 - 视频 三个 pipeline + 一个 player 匹配合成语音与视频
+
